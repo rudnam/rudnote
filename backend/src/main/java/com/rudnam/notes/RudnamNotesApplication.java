7a1c72a96af7a6c67d7cff80ce7a92ca60dc1e28
rudnam <francisalbarracin2@gmail.com>
2025-07-28 15:46:34 +0800

    Set up project

---
+++ backend/src/main/java/com/rudnam/notes/RudnamNotesApplication.java
@@ -0,0 +1,13 @@
+package com.rudnam.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudnamNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RudnamNotesApplication.class, args);
+	}
+
+}
