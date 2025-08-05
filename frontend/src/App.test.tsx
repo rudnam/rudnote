49fb7ac1a5685d2b8fd87b3aee741250c0624543
rudnam <francisalbarracin2@gmail.com>
2025-08-05 20:41:37 +0800

    Wrap App with Router

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,13 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
 import App from "./App";
+import { BrowserRouter } from "react-router";
 
 describe("App", () => {
   it("renders", () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>);
   });
 });
